5fce7a7816df6782887a8a4eb9a3591f24db6384
claytonfrench <frenchclayton26@gmail.com>
2017-04-15 10:11:16 -0400

    Added constructor & changed to accomodate rectangular maze

--- src/BacktrackMaze.cpp
+++ src/BacktrackMaze.cpp
@@ -1,14 +1,27 @@
 #include "BacktrackMaze.h"
 
 
+BacktrackMaze::BacktrackMaze(int width, int length) {
+	this->width = width;
+	this->length = length;
+	startingRow = (rand() % (int)(width));
+	startingCol = (rand() % (int)(length));
+	
+	maze = new Tile*[width];
+	for(int x = 0 ; x < width; x++)
+		maze[x] = new Tile[length];
+	
+	
+}
+
 void BacktrackMaze::init_maze() {
-	for(int x = 0; x < this->size; x++) {
-		for(int y = 0; x < this->size; x++) {
-			maze[x][y].passed = false;
-			maze[x][y].north = false;
-			maze[x][y].south = false;
-			maze[x][y].east = false;
-			maze[x][y].west = false;
+	for(int row = 0; row < this->width; row++) {
+		for(int col = 0; col < this->length; col++) {
+			maze[row][col].passed = false;
+			maze[row][col].north = false;
+			maze[row][col].south = false;
+			maze[row][col].east = false;
+			maze[row][col].west = false;
 		}
 	}
 }
@@ -21,9 +34,9 @@ char BacktrackMaze::find_available_neighbor(int row, int col) {
 	for(int i = 0; i < 3; ++i) {
 		if(tilesAround[i] == 'N' && maze[row-1][col].passed == false && row > 0)
 			return 'N';
-		else if(tilesAround[i] == 'S' && maze[row+1][col].passed == false && row+1 < size)
+		else if(tilesAround[i] == 'S' && maze[row+1][col].passed == false && row+1 < width)
 			return 'S';
-		else if(tilesAround[i] == 'E' && maze[row][col+1].passed == false && col < size-1)
+		else if(tilesAround[i] == 'E' && maze[row][col+1].passed == false && col < length-1)
 			return 'E';
 		else if(tilesAround[i] == 'W' && maze[row][col-1].passed == false && col > 0)
 			return 'W';
--- src/BacktrackMaze.h
+++ src/BacktrackMaze.h
@@ -3,21 +3,24 @@
 
 using namespace std;
 
-struct Tile {
-			bool passed;
-			bool north, south, east, west;
-			bool removeNorthWall, removeSouthWall, removeEastWall, removeWestWall;
-		};
+struct Tile 
+{
+	bool passed;
+	bool north, south, east, west;
+	bool removeNorthWall, removeSouthWall, removeEastWall, removeWestWall;
+};
 
-class BacktrackMaze {
-	int size;
-	int startingRow = (rand() % (int)(size));
-	int startingCol = (rand() % (int)(size));
-	Tile** maze = new Tile*[size];
+class BacktrackMaze 
+{
+	int width, length;
+	int startingRow;
+	int startingCol;
+	Tile** maze;
 	
 	public:
 		void init_maze();
 		char find_available_neighbor(int, int);
 		void print_grid();
 		void generate_maze(int, int);	
+		BacktrackMaze(int, int);
 };
