9d4fbeaf7e6725f8b2f7b533715dde09abbf4c98
malkiewiczm <malkiewiczm@users.noreply.github.com>
2017-04-16 03:13:34 -0400

    Documentation for MazeTile

--- src/MazeTile.cpp
+++ src/MazeTile.cpp
@@ -1,5 +1,14 @@
 #include "MazeTile.h"
 
+/**
+ * @brief defines a cell for a tile in a maze
+ * @param north is false if there is a wall above the cell
+ * @param south is false if there is a wall below the cell
+ * @param east is false if there is a wall to the right of the cell
+ * @param west is false if there is a wall to the left of the cell
+ * @param x is the x-coordinate of the cell in terms of tiles
+ * @param y is the y-coordinate of the cell in terms of tiles
+ */
 MazeTile::MazeTile(bool north, bool south, bool east,
 				   bool west, int x, int y)
 {
@@ -12,6 +21,12 @@ MazeTile::MazeTile(bool north, bool south, bool east,
 	this->y = y;
 }
 
+/**
+ * @brief defines a cell for a tile in a maze, setting default values
+ * for walls
+ * @param x is the x-coordinate of the cell in terms of tiles
+ * @param y is the y-coordinate of the cell in terms of tiles
+ */
 MazeTile::MazeTile(int x, int y)
 {
 	this->north = true;
