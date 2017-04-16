f5175a43f4628127e49ae107219ef56c2c750b92
malkiewiczm <malkiewiczm@users.noreply.github.com>
2017-04-16 01:40:41 -0400

    Documentation for Color

--- src/Color.cpp
+++ src/Color.cpp
@@ -1,5 +1,8 @@
 #include "Color.h"
 
+/**
+ * @brief creates a black color / pixel
+ */
 Color::Color()
 {
 	this->red = 0;
@@ -7,6 +10,12 @@ Color::Color()
 	this->blue = 0;
 }
 
+/**
+ * @brief creates specified color
+ * @param r is the red value for the color
+ * @param g is the green value for the color
+ * @param b is the blue value for the color
+ */
 Color::Color(byte r, byte g, byte b)
 {
 	this->red = r;
@@ -14,19 +23,28 @@ Color::Color(byte r, byte g, byte b)
 	this->blue = b;		
 }
 
-// returns average of each color channel
+/**
+ * @brief gets the average of each color channel
+ * @return the arithmetic mean of the color channels
+ */
 byte Color::grayscale_average()
 {
 	return (this->red + this->green + this->blue) / 3;
 }
 
-// checks if it is `close enough` to black
+/**
+ * @brief checks if the color is darker than medium gray
+ * @return true if the color is darker than gray, or else false
+ */
 bool Color::is_black()
 {
 	return this->grayscale_average() < 127;
 }
 
-// checks if it is `close enough` to white
+/**
+ * @brief checks if the color is lighter than medium gray
+ * @return true if the color is lighter than gray, or else false
+ */
 bool Color::is_white()
 {
 	return this->grayscale_average() > 127;
