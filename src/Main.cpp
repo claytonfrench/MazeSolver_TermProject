1a9f689d7b1a46ab9beaf0de430387ab4ea83d75
jakekirstein <jakekirstein@ufl.edu>
2017-04-16 01:59:02 -0400

    Backtrack test

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include "BFSMaze.h"
 #include "MazeSolver.h"
+#include "BacktrackMaze.h"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
 	try {
-		Maze *maze = new BFSMaze(5, 5);
+		Maze *maze = new BacktrackMaze(10, 10);
 		MazeSolver solver(maze);
+		maze->print_maze();
+		maze->to_bitmap()->write_file();
 		solver.solve_maze();
 		solver.print_maze();
 		solver.to_bitmap()->write_file();
